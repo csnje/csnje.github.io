92ad14e6740b5d123859c0fd2f061e53c4282f55
Chris Snape-Jenkinson <45893354+csnje@users.noreply.github.com>
2023-06-24 16:41:46 +1000

    Updated build for minor changes

--- wasm-barnsley/pkg/wasm_barnsley.d.ts
+++ wasm-barnsley/pkg/wasm_barnsley.d.ts
@@ -9,10 +9,10 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly main: () => void;
-  readonly __wbindgen_malloc: (a: number) => number;
-  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9c20bd527b170a05: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke0_mut__h50a5c0c7e961b283: (a: number, b: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
   readonly __wbindgen_start: () => void;
 }
@@ -36,4 +36,4 @@ export function initSync(module: SyncInitInput): InitOutput;
 *
 * @returns {Promise<InitOutput>}
 */
-export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
--- wasm-barnsley/pkg/wasm_barnsley_bg.wasm.d.ts
+++ wasm-barnsley/pkg/wasm_barnsley_bg.wasm.d.ts
@@ -2,9 +2,9 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function main(): void;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9c20bd527b170a05(a: number, b: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h50a5c0c7e961b283(a: number, b: number): void;
 export function __wbindgen_exn_store(a: number): void;
 export function __wbindgen_start(): void;
